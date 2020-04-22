f80d62c75d3f1164a1d41715ac92c4f34a2b4775
Owen Voke <development@voke.dev>
2020-04-22 16:58:05 +0100

    Update default Git details

--- src/git-setup.sh
+++ src/git-setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 _setup_git() {
     git_remote="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-    git config --global user.name "${INPUT_COMMIT_USER_NAME}"
-    git config --global user.email "${INPUT_COMMIT_USER_EMAIL}"
+    git config --global user.name "${INPUT_COMMIT_USER_NAME:-Homebrew Updater Bot}"
+    git config --global user.email "${INPUT_COMMIT_USER_EMAIL:-action@github.com}"
 
     git remote set-url --push origin "${git_remote}"
 }
